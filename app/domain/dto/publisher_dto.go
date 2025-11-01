eac2e559d01cadd76431090e03a23920499aec71
Zukron Alviandy <zukronalviandy@gmail.com>
2025-11-01 13:29:14 +0700

    feat: add some validation in publisher

--- app/domain/dto/publisher_dto.go
+++ app/domain/dto/publisher_dto.go
@@ -6,8 +6,6 @@ type PublisherDto struct {
 	ID      *uuid.UUID
 	Name    string `binding:"required_without=ID" json:"name"`
 	Address string `binding:"required_without=ID" json:"address"`
-	// TODO: validate phone start with 08
-	Phone string `binding:"required_without=ID" json:"phone"`
-	// TODO: validate website is valid url
-	Website string `binding:"required_without=ID" json:"website"`
+	Phone   string `binding:"required_without=ID,startswith=08" json:"phone"`
+	Website string `binding:"required_without=ID,url" json:"website"`
 }
